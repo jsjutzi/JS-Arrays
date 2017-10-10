2ebca426b7d6dd8ed89573df0d429f4a889edcff
Jack <jsjutzi@gmail.com>
2017-10-10 18:07:18 -0500

    Incomplete

--- arraysPractice.js
+++ arraysPractice.js
@@ -4,7 +4,9 @@
 var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
 
-  //Code Here
+  const first = (arr) => {
+    return arr[0];
+  }
 
 
 //Next problem
@@ -15,7 +17,10 @@ var arr = [40,50,60];
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item in the given array.
 
 
-  //Code Here
+  const last = (arr) =>{
+    return arr[arr.length - 1];
+
+  }
 
 
 //Next Problem
@@ -24,7 +29,11 @@ var arr = [40,50,60];
 var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
-  //Code Here
+  const looper = (family) => {
+    for (var i = 0; i < family.length; i++){
+      alert(family[i]);
+    }
+  }
 
 
 //Next problem
@@ -34,7 +43,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
-  //Code Here
+  const reversedLooper = (letters) => {
+      for (var i = letters.length - 1; i >= 0; i--){
+        alert(letters[i]);
+
+      }
+  }
 
 
 //Next Problem
@@ -43,7 +57,14 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
-  //Code Here
+  const evenFinder = (nums) => {
+    for (var i = 0; i < nums.length; i++){
+      if (nums[i] % 2 !== 0){
+        nums.splice(i, 1);
+      }
+    }
+    return nums;
+  }
 
 
 
@@ -72,7 +93,9 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 
 
 
-  //Code Here
+  // const divider = (numbersArray){
+
+  // }
 
 
 //Next Problem
